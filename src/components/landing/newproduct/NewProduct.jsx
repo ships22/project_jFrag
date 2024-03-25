d5c2f9a911aed0b81b68a535911d65b4150267d7
ships22 <sshipsss@gmail.com>
2024-03-25 04:27:14 +0100

    new product cart init

--- src/components/landing/newproduct/NewProduct.jsx
+++ src/components/landing/newproduct/NewProduct.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import styles from './newproduct.module.css'
-// {
-//     id: 4,
-//     name: 'Nuit de noel',
-//     description: 'Eau de perfum',
-//     category: 'Unisex',
-//     size: '100ml',
-//     price: 65,
-//     photo: nuit,
-//   }
+
 export const NewProduct = ({ product }) => {
   const { id, name, description, category, size, price, photo } = product
   return (
